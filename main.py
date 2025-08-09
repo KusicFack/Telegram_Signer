f3265820cd985e7fb704096de0120e1c08fb3f91
KusicFack <129599166+KusicFack@users.noreply.github.com>
2025-08-09 09:05:05 +0800

    Test passed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from config import api_id, api_hash
 from telethon import TelegramClient
-import asyncio, os
+import asyncio, os, datetime
 
 log_file = open(os.path.join(os.path.dirname(__file__), "log.txt"),'a',encoding="UTF-8")
 
@@ -8,15 +8,17 @@ def dual_print(text):
     print(text)
     log_file.write(text+'\n')
 
+# KusicFack 处替换自定义名字，第一次使用脚本时需要经历完整的登录流程，然后会在脚本目录下生成“自定义名字.session”文件来保存登录凭证
 client = TelegramClient("KusicFack",api_id=api_id, api_hash=api_hash, proxy=("SOCKS5","127.0.0.1",7897))
 
 async def main():
+    dual_print(("脚本执行时间："+ datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")).center(os.get_terminal_size().columns-7,"="))
     try:
         user = await client.get_me()
-        dual_print(f"已登录：{user.name}，准备签到...")
+        dual_print(f"已登录：{user.first_name}{user.last_name}，准备签到...")
 
         # 记录当前最新消息
-        last_message = await anext(client.iter_messages(from_user="@ure_best_bot", limit=1))
+        last_message = await anext(client.iter_messages("@ure_best_bot", limit=1))
 
         # 开始签到
         dual_print("开始签到...")
@@ -28,11 +30,12 @@ async def main():
             await asyncio.sleep(0.5)
             new_message = await anext(client.iter_messages("@ure_best_bot", limit=1))
             if new_message.id > last_message.id:
-                dual_print("签到结果：", new_message)
+                dual_print("签到结果："+ new_message.text)
                 break
             if _ == 4: raise Exception("未收到签到结果回复！")
     except Exception as e:
         dual_print("签到失败！错误信息："+str(e))
+    dual_print("")
 
 with client:
     client.loop.run_until_complete(main())
